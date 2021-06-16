c2e85ea3b2ac7319d8aaca6535aa779067b2d6cf
Tann1 <gadkaritanishq@gmail.com>
2021-06-16 01:40:38 -0700

    Finished textbox entry button; started canvas rendering

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
+const _width = 1000;
+
+
 // JS script goes here
+let form = document.querySelector('#nBarsInput'); //# = reference to ID
+//let form = document.getElementById('nForm');
+
+let canvas = document.querySelector('.zdog-canvas');
+let ctx = canvas.getContext('2d'); 
+let nTextBox = document.getElementById('nTextBox');
+let nSubmitButton = document.getElementById('nSubmit');
+let nArr = [];
+let nVal = [];
+nSubmitButton.addEventListener('click', () => {
+    // do stuff with textbox.value
+    console.log(nTextBox.value);
+    for(let i = 0; i < nTextBox.value; ++i){
+        let randVal = 2 * Math.floor((Math.random()*100) + 1);
+        let rect = new Zdog.Rect({
+            addTo: illo,
+            width: _width / nTextBox.value,
+            height: randVal,
+            color: 'black',
+            fill: true,
+            translate: {y: 0, x :  i * (_width/nTextBox.value)}, 
+        });
+        nArr.push(rect);
+        nVal.push(randVal);
+        illo.updateRenderGraph();
+    }
+    console.log(nVal);
+});
+
+let illo = new Zdog.Illustration({
+    element: ".zdog-canvas",
+    centered: false
+});
+
+
+
