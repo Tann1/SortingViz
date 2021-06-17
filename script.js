ed713fdb4bada873d6e160067ac0924fa8f9b10a
Tann1 <gadkaritanishq@gmail.com>
2021-06-16 23:39:06 -0700

    Fixed bar illustration, need to work on bar text next

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const _width = 1000;
-
-
 // JS script goes here
 let form = document.querySelector('#nBarsInput'); //# = reference to ID
 //let form = document.getElementById('nForm');
@@ -13,28 +10,44 @@ let nArr = [];
 let nVal = [];
 nSubmitButton.addEventListener('click', () => {
     // do stuff with textbox.value
-    console.log(nTextBox.value);
-    for(let i = 0; i < nTextBox.value; ++i){
-        let randVal = 2 * Math.floor((Math.random()*100) + 1);
-        let rect = new Zdog.Rect({
-            addTo: illo,
-            width: _width / nTextBox.value,
+    nArr = [];
+    nVal = [];
+    let barWidth = (canvas.width/nTextBox.value);
+    let illo = new Zdog.Illustration({
+        element: ".zdog-canvas",
+        centered: false
+    });
+    let randVal = Math.floor((Math.random()*400) + 1);
+    nVal.push(randVal);
+    let rect = new Zdog.Rect({
+        addTo: illo,
+        width: barWidth,
+        height: randVal,
+        color: 'black',
+        fill: true,
+        translate: {y: canvas.height - randVal / 2, x : barWidth/2}});
+    illo.updateRenderGraph();
+
+    for(let i = 1; i < nTextBox.value; ++i){
+        randVal = Math.floor((Math.random()*400) + 1);
+        rect.copy({
             height: randVal,
-            color: 'black',
-            fill: true,
-            translate: {y: 0, x :  i * (_width/nTextBox.value)}, 
+            color: 'brown',
+            translate: {y: canvas.height - randVal/ 2, x :  (i * barWidth) + barWidth/2}
         });
+        
         nArr.push(rect);
         nVal.push(randVal);
         illo.updateRenderGraph();
     }
     console.log(nVal);
+    ctx.fillStyle = 'black';
+    ctx.font = '48px impact';
+    ctx.fillText('test', 500, 500);
 });
 
-let illo = new Zdog.Illustration({
-    element: ".zdog-canvas",
-    centered: false
-});
+
+
 
 
 
