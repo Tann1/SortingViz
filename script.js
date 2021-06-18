b8584f3b144db9448e9f1415ad4b451949ab85f1
Tann1 <gadkaritanishq@gmail.com>
2021-06-18 01:10:09 -0700

    Added text for the bars.

--- script.js
+++ script.js
@@ -1,53 +1,77 @@
 // JS script goes here
 let form = document.querySelector('#nBarsInput'); //# = reference to ID
-//let form = document.getElementById('nForm');
 
-let canvas = document.querySelector('.zdog-canvas');
+
+
+let canvas = document.querySelector('.zdog-canvas'); 
 let ctx = canvas.getContext('2d'); 
 let nTextBox = document.getElementById('nTextBox');
 let nSubmitButton = document.getElementById('nSubmit');
-let nArr = [];
-let nVal = [];
-nSubmitButton.addEventListener('click', () => {
-    // do stuff with textbox.value
-    nArr = [];
-    nVal = [];
-    let barWidth = (canvas.width/nTextBox.value);
-    let illo = new Zdog.Illustration({
-        element: ".zdog-canvas",
-        centered: false
-    });
-    let randVal = Math.floor((Math.random()*400) + 1);
-    nVal.push(randVal);
-    let rect = new Zdog.Rect({
-        addTo: illo,
-        width: barWidth,
-        height: randVal,
-        color: 'black',
-        fill: true,
-        translate: {y: canvas.height - randVal / 2, x : barWidth/2}});
-    illo.updateRenderGraph();
+let nArr = []; //stores ZdogRect object for each bar
+let nVal = []; //stores the array that needs to be sorted 
+let unsorted = 'pink'
+let sorted = 'brown'
+let sorting = 'yellow'
 
-    for(let i = 1; i < nTextBox.value; ++i){
-        randVal = Math.floor((Math.random()*400) + 1);
-        rect.copy({
-            height: randVal,
-            color: 'brown',
-            translate: {y: canvas.height - randVal/ 2, x :  (i * barWidth) + barWidth/2}
-        });
-        
-        nArr.push(rect);
-        nVal.push(randVal);
-        illo.updateRenderGraph();
-    }
-    console.log(nVal);
-    ctx.fillStyle = 'black';
-    ctx.font = '48px impact';
-    ctx.fillText('test', 500, 500);
+
+nSubmitButton.addEventListener('click', () => {  // generate graph when submit button is clicked
+    
+    generateNums(nTextBox.value);
+    createCanvas(nTextBox.value);
 });
 
 
+function generateNums(size) {  // generates array of size ntextBox.value, populated with random numbers
+    nVal = [];  // reset array
+    for (let i = 0; i < size; ++i){
+        let randVal = Math.floor((Math.random() * 0.55 * canvas.height) + (0.40 * canvas.height));
+        nVal.push([randVal, unsorted]);
+    }
+}
 
+// do drawing stuff based on given array and N value
+
+function createCanvas(size) { 
+    let barWidth = (canvas.width/size);
+    let illo = new Zdog.Illustration({  //main parent object of 
+        element: ".zdog-canvas",
+        centered: false
+    });
+
+   
+
+    let rect = new Zdog.Rect({  // create first bar
+        addTo: illo,
+        width: barWidth,
+        height: nVal[0][0],
+        color: nVal[0][1],
+        fill: true,
+        stroke: 2,
+        translate: {y: canvas.height - nVal[0][0] / 2, x : barWidth/2}});
+    illo.updateRenderGraph();
+
+    for(let i = 1; i < size; ++i){  // create rest of the bars by copying the first bar
+        rect.copy({
+            height: nVal[i][0],
+            color: nVal[i][1],
+            translate: {y: canvas.height - nVal[i][0]/ 2, x :  (i * barWidth) + barWidth/2}
+        });
+        
+        
+        //nArr.push(rect);
+        illo.updateRenderGraph();
+        
+         /* text setup*/
+    }
+    for (let i = 0; i < size; ++i) {
+        ctx.fillStyle = 'black';
+        ctx.textAlign = 'center';
+        ctx.font =  (0.50 * barWidth) + 'px monospace';
+        console.log((0.75 * barWidth));
+        ctx.fillText(nVal[i][0], (i * barWidth) + (barWidth/2), 490);
+    }
+        
+}
 
 
 
